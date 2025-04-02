e6be845d337f4652f81aa3263a5224cfe4c777fd
Matija Tudan <matija.tudan@nulix.hr>
2025-04-02 22:17:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,37 +6,39 @@ set -o pipefail
 STEP_NAME=$(echo "$INPUT_STEP_NAME" | tr '-' '_')
 MACHINE="$INPUT_MACHINE"
 DISTRO="$INPUT_DISTRO"
+COMPOSE_FILE="$INPUT_COMPOSE_FILE"
 NULIX_OS_VER="1.3.1"
 
 echo "================================================"
 echo "============ Action Input Variables ============"
 echo "================================================"
-echo "Step: $STEP_NAME"
-echo "Machine: $MACHINE"
-echo "Distro: $DISTRO"
+echo "STEP:             $STEP_NAME"
+echo "MACHINE:          $MACHINE"
+echo "DISTRO:           $DISTRO"
+echo "COMPOSE FILE:     $COMPOSE_FILE"
 echo "NULIX OS version: $NULIX_OS_VER"
 echo "================================================"
 
 do_fetch_base_os() {
-  ls -lh
-  # source /nulix-os-venv/bin/activate
-  # west init -m https://github.com/nulix/nulix-os.git nulix-os
-  # cd nulix-os
-  # west update
-  # MACHINE=$MACHINE DISTRO=$DISTRO source tools/setup-environment
-  # cd build/deploy/$MACHINE
-  # wget https://files.0xff.com.hr/$MACHINE/$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz
-  # wget https://files.0xff.com.hr/$MACHINE/$OSTREE_REPO.tar.gz
-  # tar xzf $OSTREE_REPO.tar.gz
-  # mv -v $OSTREE_REPO ../../../rootfs
-  # mv -v $OSTREE_ROOTFS-*.tar.gz ../../../rootfs
-  # rm $OSTREE_REPO.tar.gz
-  # cd ../../..
-  # rm -f rootfs/$OSTREE_REPO.tar.gz
+  source /nulix-os-venv/bin/activate
+  west init -m https://github.com/nulix/nulix-os.git nulix-os
+  cd nulix-os
+  west update
+  MACHINE=$MACHINE DISTRO=$DISTRO source tools/setup-environment
+  cd build/deploy/$MACHINE
+  wget https://files.0xff.com.hr/$MACHINE/$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz
+  wget https://files.0xff.com.hr/$MACHINE/$OSTREE_REPO.tar.gz
+  tar xzf $OSTREE_REPO.tar.gz
+  mv -v $OSTREE_REPO ../../../rootfs
+  mv -v $OSTREE_ROOTFS-*.tar.gz ../../../rootfs
+  rm $OSTREE_REPO.tar.gz
+  cd ../../..
+  rm -f rootfs/$OSTREE_REPO.tar.gz
 }
 
 do_inject_apps() {
   cd nulix-os
+  cp ../$COMPOSE_FILE rootfs/apps
   MACHINE=$MACHINE DISTRO=$DISTRO source tools/setup-environment
   OSTREE_COMMIT_MSG="Added custom compose apps"
   nulix build ostree-repo
