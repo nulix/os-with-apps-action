c179f6369b770e0953a4cd5baf4f5c20f8f7dee1
Matija Tudan <matija.tudan@nulix.hr>
2025-04-02 13:20:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,22 @@ set -o pipefail
 
 STEP_NAME="$INPUT_STEP_NAME"
 MACHINE="$INPUT_MACHINE"
+DISTRO="$INPUT_DISTRO"
 
+echo "================================================="
+echo "================ Input variables ================"
+echo "================================================="
 echo "Running step: $STEP_NAME"
 echo "Machine: $MACHINE"
+echo "Distro: $DISTRO"
+echo "================================================="
 
 do_fetch-base-os() {
   source /nulix-os-venv/bin/activate
   west init -m https://github.com/nulix/nulix-os.git nulix-os
   cd nulix-os
   west update
-  MACHINE=$MACHINE source tools/setup-environment
+  MACHINE=$MACHINE DISTRO=$DISTRO source tools/setup-environment
   cd build/deploy/$MACHINE
   wget https://files.0xff.com.hr/$MACHINE/$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz
   wget https://files.0xff.com.hr/$MACHINE/$OSTREE_REPO.tar.gz
@@ -29,7 +35,7 @@ do_fetch-base-os() {
 
 do_inject-apps() {
   cd nulix-os
-  MACHINE=$MACHINE source tools/setup-environment
+  MACHINE=$MACHINE DISTRO=$DISTRO source tools/setup-environment
   OSTREE_COMMIT_MSG="Added custom compose apps"
   nulix build ostree-repo
 }
