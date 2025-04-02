75e775b06ec531e6c6894c3a88d4336ebf1cabd9
Matija Tudan <matija.tudan@nulix.hr>
2025-04-02 22:11:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,20 +18,21 @@ echo "NULIX OS version: $NULIX_OS_VER"
 echo "================================================"
 
 do_fetch_base_os() {
-  source /nulix-os-venv/bin/activate
-  west init -m https://github.com/nulix/nulix-os.git nulix-os
-  cd nulix-os
-  west update
-  MACHINE=$MACHINE DISTRO=$DISTRO source tools/setup-environment
-  cd build/deploy/$MACHINE
-  wget https://files.0xff.com.hr/$MACHINE/$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz
-  wget https://files.0xff.com.hr/$MACHINE/$OSTREE_REPO.tar.gz
-  tar xzf $OSTREE_REPO.tar.gz
-  mv -v $OSTREE_REPO ../../../rootfs
-  mv -v $OSTREE_ROOTFS-*.tar.gz ../../../rootfs
-  rm $OSTREE_REPO.tar.gz
-  cd ../../..
-  rm -f rootfs/$OSTREE_REPO.tar.gz
+  ls -lh
+  # source /nulix-os-venv/bin/activate
+  # west init -m https://github.com/nulix/nulix-os.git nulix-os
+  # cd nulix-os
+  # west update
+  # MACHINE=$MACHINE DISTRO=$DISTRO source tools/setup-environment
+  # cd build/deploy/$MACHINE
+  # wget https://files.0xff.com.hr/$MACHINE/$OSTREE_ROOTFS-$NULIX_OS_VER.tar.gz
+  # wget https://files.0xff.com.hr/$MACHINE/$OSTREE_REPO.tar.gz
+  # tar xzf $OSTREE_REPO.tar.gz
+  # mv -v $OSTREE_REPO ../../../rootfs
+  # mv -v $OSTREE_ROOTFS-*.tar.gz ../../../rootfs
+  # rm $OSTREE_REPO.tar.gz
+  # cd ../../..
+  # rm -f rootfs/$OSTREE_REPO.tar.gz
 }
 
 do_inject_apps() {
